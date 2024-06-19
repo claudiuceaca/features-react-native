a8908a6a43d5abad67c08632590e1ae17a940dcd
Iosif-Claudiu Ceaca <ceacaiosifclaudiu@gmail.com>
2024-06-19 23:54:38 +0300

    refactor: change button and gradient wrapper comp

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,40 +1,21 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import { TouchableOpacity, Text, StyleSheet, ViewStyle, TextStyle, StyleProp } from 'react-native';
+import GradientWrapper from './GradientWrapper';
 import { DEVICE_SIZE } from '@/constants/utils';
 
 interface ButtonProps {
     text: string;
     onPress?: () => void;
-    buttonStyle?: object;
-    textStyle?: object;
+    buttonStyle?: StyleProp<ViewStyle>;
+    textStyle?: StyleProp<TextStyle>;
 }
 
-const Button = (props: ButtonProps) => {
-    const { text, onPress, buttonStyle, textStyle } = props;
-
-    const generateRandomGradient = () => {
-        const colors = [
-            ['#FF512F', '#DD2476'],
-            ['#FFC371', '#FF5F6D'],
-            ['#02AAB0', '#00CDAC'],
-            ['#DA22FF', '#9733EE'],
-            ['#8E2DE2', '#4A00E0'],
-        ];
-
-        const randomIndex = Math.floor(Math.random() * colors.length);
-        return colors[randomIndex];
-    };
-
-    const randomGradientColors = generateRandomGradient();
-
+const Button: React.FC<ButtonProps> = ({ text, onPress, buttonStyle, textStyle }) => {
     return (
         <TouchableOpacity onPress={onPress} style={{ width: '100%' }}>
-            <LinearGradient
-                style={[styles.button, buttonStyle]}
-                colors={randomGradientColors}>
+            <GradientWrapper style={[styles.button, buttonStyle]}>
                 <Text style={[styles.text, textStyle]}>{text}</Text>
-            </LinearGradient >
+            </GradientWrapper>
         </TouchableOpacity>
     );
 };
@@ -42,17 +23,10 @@ const Button = (props: ButtonProps) => {
 const styles = StyleSheet.create({
     button: {
         borderRadius: 8,
-        overflow: 'hidden',
-        alignItems: 'center',
-        justifyContent: 'center',
         height: 40,
-        width: '100%'
-    },
-    gradient: {
-        flex: 1,
-        borderRadius: 8,
-        alignItems: 'center',
+        width: '100%',
         justifyContent: 'center',
+        alignItems: 'center',
     },
     text: {
         fontSize: DEVICE_SIZE.SMALL_DEVICE ? 16 : (DEVICE_SIZE.MEDIUM_DEVICE ? 18 : 20),
