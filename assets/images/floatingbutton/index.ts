e8d0b507014b76822255814b37ca584b6bcd121f
Iosif-Claudiu Ceaca <ceacaiosifclaudiu@gmail.com>
2024-06-20 18:34:10 +0300

    style: add and export assets

---
+++ assets/images/floatingbutton/index.ts
@@ -0,0 +1,4 @@
+export { default as Save } from './save.svg';
+export { default as Edit } from './edit.svg';
+export { default as Send } from './send.svg';
+export { default as Close } from './close.svg';
---
+++ assets/images/profile/index.ts
@@ -0,0 +1,12 @@
+export { default as Profile1 } from './Profile1.svg';
+export { default as Profile2 } from './Profile2.svg';
+export { default as Profile3 } from './Profile3.svg';
+export { default as Profile4 } from './Profile4.svg';
+export { default as Profile5 } from './Profile5.svg';
+export { default as Profile6 } from './Profile6.svg';
+export { default as Profile7 } from './Profile7.svg';
+export { default as Profile8 } from './Profile8.svg';
+export { default as Profile9 } from './Profile9.svg';
+export { default as Profile10 } from './Profile10.svg';
+export { default as Profile11 } from './Profile11.svg';
+export { default as Profile12 } from './Profile12.svg';
