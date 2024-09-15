09f036af97e37ef6b8d753e1b78f502989bc8d44
Iosif-Claudiu Ceaca <ceacaiosifclaudiu@gmail.com>
2024-09-16 01:54:39 +0300

    refactor: remove unused text import from flatlist synced

--- app/FLatListSynced/index.tsx
+++ app/FLatListSynced/index.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StyleSheet, View } from 'react-native'
 import FlatListSynced from './FLatListSynced'
 
 const App = () => {
